945fb13a56f54652889da73fd19a78b74263a661
marcello505 <marcellohaddeman@gmail.com>
2020-05-14 05:25:55 +0200

    changed colour scheme

--- config.h
+++ config.h
@@ -7,11 +7,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[]       = "monospace:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#5F4F72";
+static const char col_gray2[]       = "#292045";
+static const char col_gray3[]       = "#BBA3B1";
+static const char col_gray4[]       = "#715F9E";
+static const char col_cyan[]        = "#E1DAE9";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
